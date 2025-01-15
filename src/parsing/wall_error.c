d6f189486d60227edef2b5c4fe6df4274cc64e60
Romain Bouquet <romain.bouquet05@gmail.com>
2025-01-15 12:35:31 +0100

    wall_error

--- src/parsing/wall_error.c
+++ src/parsing/wall_error.c
@@ -6,7 +6,7 @@
 /*   By: rbouquet <rbouquet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 11:54:14 by romain            #+#    #+#             */
-/*   Updated: 2025/01/15 12:30:58 by rbouquet         ###   ########.fr       */
+/*   Updated: 2025/01/15 12:35:28 by rbouquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ bool	check_column(char **column, int y, int x, int size_y)
 			while (y < size_y && column[y][x] == ' ')
 				y++;
 			if (y < size_y && column[y][x] != '1')
-				return (false)
+				return (false);
 		}
 		if (y < size_y)
 			y++;
@@ -37,64 +37,61 @@ bool	check_column(char **column, int y, int x, int size_y)
 	return (true);
 }
 
-int	vertical_check(t_map *map, int y, int x)
+bool	vertical_check(t_map *map, int y, int x)
 {
 	while (map->map_tab[y][x])
 	{
 		if (!check_column(map->map_tab, y, x, map->column_map))
-			return (0);
+			return (false);
 		x++;
 	}
-	return (1);
+	return (true);
 }
 
-int	check_line(t_map *map, char *line, int x)
+bool	check_line(t_map *map, char *line, int x)
 {
 	while (line[x])
 	{
 		if (x == 0 && !is_valid_char(line[x], "1 "))
-			return (0);
+			return (false);
 		if (!is_valid_char(line[x], "10NSEW "))
 			return (print_error("INVALID_CHARACTER"), 1);
 		if (line[x] == ' ')
 		{
 			if (x > 0 && line[x - 1] != '1')
-				return (0);
+				return (false);
 			while (line[x] && line[x] == ' ')
 				x++;
 			if (line[x] && line[x] != '1')
-				return (0);
+				return (false);
 		}
 		if (line[x])
 			x++;
 	}
 	if (!is_valid_char(line[x - 1], "1 "))
-		return (0);
-	return (1);
+		return (false);
+	return (true);
 }
 
-int	horizontal_check(t_map *map, int y, int x)
+bool	horizontal_check(t_map *map, int y, int x)
 {
 	while (map->map_tab[y])
 	{
 		if (!check_line(map, map->map_tab[y], x))
-			return (0);
+			return (false);
 		y++;
 	}
-	return (1);
+	return (true);
 }
 
-int	ft_wall_error(t_map *map)
+bool	ft_wall_error(t_map *map)
 {
-	int y;
-	int x;
+	int	y;
+	int	x;
 
 	y = 0;
 	x = 0;
 	if (!horizontal_check(map, y, x) || !vertical_check(map, y, x))
-	{
-		free_rgb_texture(map);
-		return (0);
-	}
-	return (1);
+		return (false);
+	return (true);
 }
